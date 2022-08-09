e68b38b16d33adb3260769849b4792137c0a4605
FlorianBury <FlorianBury@github.com>
2022-08-09 16:14:45 +0200

    Piecewise linear implemented in the nonclosure

--- nonclosure.py
+++ nonclosure.py
@@ -4,6 +4,7 @@ import copy
 import json
 import functools
 import numpy as np
+from IPython import embed
 import logging
 import ROOT
 
@@ -23,66 +24,90 @@ class NonClosureDY:
         return self.content.keys()
 
     @staticmethod
-    def _fit(coefficients):
-        return lambda x : coefficients[0] + coefficients[1] * x
+    def _correct_nom(x,coefficients):
+        return coefficients[0] + coefficients[1] * x
+        #return lambda x : coefficients[0] + coefficients[1] * x
 
     @staticmethod
-    def _shift_nom(coefficients):
-        return lambda x : coefficients[0] + coefficients[1] * x
+    def _shift1_up(x,coeffs,eigenVals,eigenVecs):
+        return (coeffs[0] + np.sqrt(eigenVals[0]) * eigenVecs[0][0]) \
+             + (coeffs[1] + np.sqrt(eigenVals[0]) * eigenVecs[1][0]) * x
+        #return lambda x : (coeffs[0] + np.sqrt(eigenVals[0]) * eigenVecs[0][0]) \
+        #                + (coeffs[1] + np.sqrt(eigenVals[0]) * eigenVecs[1][0]) * x
+    @staticmethod
+    def _shift1_down(x,coeffs,eigenVals,eigenVecs):
+        return (coeffs[0] - np.sqrt(eigenVals[0]) * eigenVecs[0][0]) \
+             + (coeffs[1] - np.sqrt(eigenVals[0]) * eigenVecs[1][0]) * x
+        #return lambda x : (coeffs[0] - np.sqrt(eigenVals[0]) * eigenVecs[0][0]) \
+        #                + (coeffs[1] - np.sqrt(eigenVals[0]) * eigenVecs[1][0]) * x
+    @staticmethod
+    def _shift2_up(x,coeffs,eigenVals,eigenVecs):
+        return  (coeffs[0] + np.sqrt(eigenVals[1]) * eigenVecs[0][1]) \
+              + (coeffs[1] + np.sqrt(eigenVals[1]) * eigenVecs[1][1]) * x
+        #return lambda x : (coeffs[0] + np.sqrt(eigenVals[1]) * eigenVecs[0][1]) \
+        #                + (coeffs[1] + np.sqrt(eigenVals[1]) * eigenVecs[1][1]) * x
+    @staticmethod
+    def _shift2_down(x,coeffs,eigenVals,eigenVecs):
+        return (coeffs[0] - np.sqrt(eigenVals[1]) * eigenVecs[0][1]) \
+             + (coeffs[1] - np.sqrt(eigenVals[1]) * eigenVecs[1][1]) * x
+        #return lambda x : (coeffs[0] - np.sqrt(eigenVals[1]) * eigenVecs[0][1]) \
+        #                + (coeffs[1] - np.sqrt(eigenVals[1]) * eigenVecs[1][1]) * x
 
-    @staticmethod
-    def _shift1_up(coeffs,eigenVals,eigenVecs):
-        return lambda x : (coeffs[0] + np.sqrt(eigenVals[0]) * eigenVecs[0][0]) \
-                        + (coeffs[1] + np.sqrt(eigenVals[0]) * eigenVecs[1][0]) * x
-    @staticmethod
-    def _shift1_down(coeffs,eigenVals,eigenVecs):
-        return lambda x : (coeffs[0] - np.sqrt(eigenVals[0]) * eigenVecs[0][0]) \
-                        + (coeffs[1] - np.sqrt(eigenVals[0]) * eigenVecs[1][0]) * x
-    @staticmethod
-    def _shift2_up(coeffs,eigenVals,eigenVecs):
-        return lambda x : (coeffs[0] + np.sqrt(eigenVals[1]) * eigenVecs[0][1]) \
-                        + (coeffs[1] + np.sqrt(eigenVals[1]) * eigenVecs[1][1]) * x
-    @staticmethod
-    def _shift2_down(coeffs,eigenVals,eigenVecs):
-        return lambda x : (coeffs[0] - np.sqrt(eigenVals[1]) * eigenVecs[0][1]) \
-                        + (coeffs[1] - np.sqrt(eigenVals[1]) * eigenVecs[1][1]) * x
+    def _getParameters(self,key,x):
+        entry = self.content[key]
+        if isinstance(entry,dict):
+            return np.array(entry['coefficients']),np.array(entry['covariance'])
+        elif isinstance(entry,list):
+            for e in entry:
+                assert 'range' in e.keys()
+                if x >= e['range'][0] and x <= e['range'][1]:
+                    return np.array(e['coefficients']),np.array(e['covariance'])
+            raise RuntimeError
+        else:
+            raise NotImplementedError
+
 
     def modify(self,h,cat,group,**kwargs):
         assert isinstance(h,ROOT.TH1)
         key = kwargs['key']
         if key not in self.categories:
             raise RuntimeError(f'Could not find key `{key}` in {self.path_json}')
-        coefficients = self.content[key]['coefficients']
-        f = self._fit(coefficients)
+        #coefficients = self.content[key]['coefficients']
+        #f = self._fit(coefficients)
         for i in range(1,h.GetNbinsX()+1):
             x = h.GetXaxis().GetBinCenter(i)
+            coefficients,_ = self._getParameters(key,x)
+            correction = self._correct_nom(x,coefficients)
             y = h.GetBinContent(i)
-            h.SetBinContent(i,y*f(x))
+            h.SetBinContent(i,y*correction)
 
     def additional(self,h,cat,group,systName,**kwargs):
         assert isinstance(h,ROOT.TH1)
         key = kwargs['key']
-        coefficients = self.content[key]['coefficients']
-        covariance   = np.array(self.content[key]['covariance'])
-        assert covariance.shape == (2,2)
-        eigenValues , eigenVectors = np.linalg.eigh(covariance)
+        #coefficients = self.content[key]['coefficients']
+        #covariance   = np.array(self.content[key]['covariance'])
+        #assert covariance.shape == (2,2)
+        #eigenValues , eigenVectors = np.linalg.eigh(covariance)
         h_shape1_up   = h.Clone(f"{h.GetName()}_{systName}_shape1_up")
         h_shape1_down = h.Clone(f"{h.GetName()}_{systName}_shape1_down")
         h_shape2_up   = h.Clone(f"{h.GetName()}_{systName}_shape2_up")
         h_shape2_down = h.Clone(f"{h.GetName()}_{systName}_shape2_down")
 
-        lambda_shape1_up    = self._shift1_up(coefficients,eigenValues,eigenVectors)
-        lambda_shape1_down  = self._shift1_down(coefficients,eigenValues,eigenVectors)
-        lambda_shape2_up    = self._shift2_up(coefficients,eigenValues,eigenVectors)
-        lambda_shape2_down  = self._shift2_down(coefficients,eigenValues,eigenVectors)
+        #lambda_shape1_up    = self._shift1_up(coefficients,eigenValues,eigenVectors)
+        #lambda_shape1_down  = self._shift1_down(coefficients,eigenValues,eigenVectors)
+        #lambda_shape2_up    = self._shift2_up(coefficients,eigenValues,eigenVectors)
+        #lambda_shape2_down  = self._shift2_down(coefficients,eigenValues,eigenVectors)
 
         for i in range(1,h.GetNbinsX()+1):
             x = h.GetXaxis().GetBinCenter(i)
             y = h.GetBinContent(i)
-            h_shape1_up.SetBinContent(i,   y * lambda_shape1_up(x))
-            h_shape1_down.SetBinContent(i, y * lambda_shape1_down(x))
-            h_shape2_up.SetBinContent(i,   y * lambda_shape2_up(x))
-            h_shape2_down.SetBinContent(i, y * lambda_shape2_down(x))
+            coefficients,covariance = self._getParameters(key,x)
+            assert covariance.shape == (2,2)
+            eigenValues , eigenVectors = np.linalg.eigh(covariance)
+            h_shape1_up.SetBinContent(i,   y * self._shift1_up(x,coefficients,eigenValues,eigenVectors))
+            h_shape1_down.SetBinContent(i, y * self._shift1_down(x,coefficients,eigenValues,eigenVectors))
+            h_shape2_up.SetBinContent(i,   y * self._shift2_up(x,coefficients,eigenValues,eigenVectors))
+            h_shape2_down.SetBinContent(i, y * self._shift2_down(x,coefficients,eigenValues,eigenVectors))
 
         return {f'{systName}_shape1Up'   : h_shape1_up,
                 f'{systName}_shape1Down' : h_shape1_down,
@@ -141,9 +166,7 @@ class NonClosureFake:
 
     def additional(self,h,cat,group,systName,**kwargs):
         assert isinstance(h,ROOT.TH1)
-        print (id(h))
         h = copy.deepcopy(h)
-        print (id(h))
         cog   = self.content[kwargs['key']]['cog']      if 'cog' in self.content[kwargs['key']].keys() else None
         nom   = self.content[kwargs['key']]['nom']      if 'nom' in self.content[kwargs['key']].keys() else None
         slope = self.content[kwargs['key']]['slope']    if 'slope' in self.content[kwargs['key']].keys() else None 
@@ -256,4 +279,86 @@ class Theory:
                     f'{systName}Down'  : h_down}
         else:
             return {}
+
+class WjetsAdHoc:
+    def __init__(self,**kwargs):
+        self._type = kwargs['type']                                                                                                                                                                        
+        if self._type not in ['linear','quadratic']:
+            raise RuntimeError
+
+
+    @property
+    def group(self):
+        return 'WJets'
+
+    def _up(self,x,xb,xm,xp):
+        if self._type == 'linear':
+            if x < xb: 
+                a = 0.2/(xb-xm)
+                b = (0.8*xb-xm)/(xb-xm)
+            else:
+                a = 0.2/(xp-xb)
+                b = (xp-1.2*xb)/(xp-xb)
+            return a * x + b 
+        if self._type == 'quadratic':
+            if abs(xb-xp) < abs(xb-xm):
+                a = 0.4/(xb-xp)**2
+                b = -0.8*xb/(xb-xp)**2
+                c = 0.8+0.4*xb**2/(xb-xp)**2
+            else:
+                a = 0.4/(xb-xm)**2
+                b = -0.8*xb/(xb-xm)**2
+                c = 0.8+0.4*xb**2/(xb-xm)**2
+            return a*x**2 + b*x + c 
+        #if self._type == 'quadratic':
+        #if self._type == 'linear':
+        #    return lambda x : 0.8 + 0.4*x
+        #if self._type == 'quadratic':
+        #    return lambda x : 1.6*x**2-1.6*x+1.2
+
+    def _down(self,x,xb,xm,xp):
+        if self._type == 'linear':
+            if x < xb: 
+                a = 0.2/(xm-xb)
+                b = (xm-1.2*xb)/(xm-xb)
+            else:
+                a = 0.2/(xb-xp)
+                b = (0.8*xb-xp)/(xb-xp)
+            return a * x + b 
+        if self._type == 'quadratic':
+            if abs(xb-xp) < abs(xb-xm):
+                a = -0.4/(xb-xm)**2
+                b = +0.8*xb/(xb-xm)**2
+                c = 1.2-0.4*xb**2/(xb-xm)**2
+            else:
+                a = -0.4/(xb-xp)**2
+                b = +0.8*xb/(xb-xp)**2
+                c = 1.2-0.4*xb**2/(xb-xp)**2
+            return a*x**2 + b*x + c
+
+    def additional(self,h,cat,group,systName,**kwargs):
+        assert isinstance(h,ROOT.TH1)
+        h_up   = h.Clone(f"{h.GetName()}_{systName}_{self._type}_up")
+        h_down = h.Clone(f"{h.GetName()}_{systName}_{self._type}_down")
+
+        xb = h.GetMean()
+        xm = h.GetXaxis().GetBinCenter(1)
+        xp = h.GetXaxis().GetBinCenter(h.GetNbinsX())
+
+        #embed()
+        for i in range(1,h.GetNbinsX()+1):
+            #x = h.GetXaxis().GetBinCenter(i)
+            x = xm + (i-1)*(xp-xm)/(h.GetNbinsX()-1)
+            y = h.GetBinContent(i)
+            h_up.SetBinContent(i,y*self._up(x,xb,xm,xp))
+            h_down.SetBinContent(i,y*self._down(x,xb,xm,xp))
+            #print (i,x,self._up(x,xb,xm,xp),self._down(x,xb,xm,xp))
+        h_up.Scale(h.Integral()/h_up.Integral())
+        h_down.Scale(h.Integral()/h_down.Integral())
+
+        return {f'{systName}_{self._type}_Up'   : h_up,
+                f'{systName}_{self._type}_Down' : h_down}
+
+
+
 
